727eff93ed621339c123c3b2ee0a90ae396f86d4
Danihmn <daniel.bezerra.mult@gmail.com>
2025-10-28 22:24:51 -0300

    Estrutura inicialmente a tela de login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,119 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFF5F5F5),
+        child: Padding(
+          padding: EdgeInsetsGeometry.only(
+            top: 80,
+            left: 20,
+            right: 20,
+            bottom: 40,
+          ),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0, 5),
+                      blurRadius: 6,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                height: 400,
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(15),
+                  child: ListView(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "Welcome",
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text("Sign In to continue"),
+                            ],
+                          ),
+                          TextButton(onPressed: () {}, child: Text("Sign Up")),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+                        child: TextFormField(
+                          autofocus: true,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+                        child: TextFormField(
+                          autofocus: true,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Forgot you password?",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
