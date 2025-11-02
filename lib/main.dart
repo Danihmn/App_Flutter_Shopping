71bb8bba843fbb9b3048163a33a5d3ad933bf22f
Danihmn <daniel.bezerra.mult@gmail.com>
2025-11-02 11:55:04 -0300

    Inicia estruturação da CART PAGE

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app_flutter_shopping/pages/login_page.dart';
+import 'package:app_flutter_shopping/pages/cart_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginPage(),
+      home: CartPage(),
     );
   }
 }
